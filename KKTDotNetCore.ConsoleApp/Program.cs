91b66c01cd76ce43e7d5c6feb66575d3ed95ad66
Kaung Khant <User@DESKTOP-DL1II6I>
2024-11-23 17:36:05 +0630

    feat: add some code

--- KKTDotNetCore.ConsoleApp/Program.cs
+++ KKTDotNetCore.ConsoleApp/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using KKTDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
 //Console.ReadLine();
-Console.ReadKey();
+
 //md => markdown
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+adoDotNetExample.Edit();
+
+Console.ReadKey();
